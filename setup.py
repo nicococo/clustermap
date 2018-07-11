47029ec21afef0f772178bb3fe361be8618fe2c4
nicococo <sinnl0s>
2018-07-11 11:15:25 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+description = "A tool to handle distributed computing on our group cluster. Based on GridMap."
+
+config = {
+    'name': 'clustermap',
+    'description': description,
+    'url': 'https://github.com/nicococo/clustermap',
+    'author': 'Nico Goernitz',
+    'author_email': 'nico.goernitz@tu-berlin.de',
+    'version': '2017.03',
+    'install_requires': ['drmaa', 'pickle'],
+    'packages': ['tilitools'],
+    'package_dir' : {'clustermap': 'clustermap'},
+    'classifiers':['Intended Audience :: Science/Research',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering',
+                   'Operating System :: POSIX',
+                   'Operating System :: Unix',
+                   'Operating System :: MacOS',
+                   'Programming Language :: Python :: 2.7']
+}
+
+setup(**config)
